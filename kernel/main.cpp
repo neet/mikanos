f951060e9a0c125e4cba218da0eb446f671f16c8
Ryo Igarashi <n33t5hin@gmail.com>
2025-05-04 18:24:02 +0900

    ファイルを分割

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,6 +1,9 @@
 #include <cstdint>
 #include <cstddef>
+
 #include "frame_buffer_config.hpp"
+#include "graphics.hpp"
+#include "font.hpp"
 
 void *operator new(size_t size, void *buf)
 {
@@ -10,58 +13,6 @@ void *operator new(size_t size, void *buf)
 void operator delete(void *obj) noexcept
 {
 }
-
-struct PixelColor
-{
-  uint8_t r, g, b;
-};
-
-class PixelWriter
-{
-public:
-  PixelWriter(const FrameBufferConfig &config) : config_{config}
-  {
-  }
-  virtual ~PixelWriter() = default;
-  virtual void Write(int x, int y, const PixelColor &c) = 0;
-
-protected:
-  uint8_t *PixelAt(int x, int y)
-  {
-    return config_.frame_buffer + 4 * (config_.pixels_per_scan_line * y + x);
-  }
-
-private:
-  const FrameBufferConfig &config_;
-};
-
-class RGBResv8BitPerColorPixelWriter : public PixelWriter
-{
-public:
-  using PixelWriter::PixelWriter;
-
-  virtual void Write(int x, int y, const PixelColor &c) override
-  {
-    auto p = PixelAt(x, y);
-    p[0] = c.r;
-    p[1] = c.g;
-    p[2] = c.b;
-  }
-};
-
-class BGRResv8BitPerColorPixelWriter : public PixelWriter
-{
-public:
-  using PixelWriter::PixelWriter;
-
-  virtual void Write(int x, int y, const PixelColor &c) override
-  {
-    auto p = PixelAt(x, y);
-    p[0] = c.b;
-    p[1] = c.g;
-    p[2] = c.r;
-  }
-};
 
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter *pixel_writer;
@@ -88,13 +39,7 @@ extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config)
     }
   }
 
-  for (int x = 0; x < 200; ++x)
-  {
-    for (int y = 0; y < 100; ++y)
-    {
-      pixel_writer->Write(x, y, {0, 255, 0});
-    }
-  }
+  WriteAscii(*pixel_writer, 0, 0, 'A', {0, 0, 0});
 
   while (1)
     __asm__("hlt");
