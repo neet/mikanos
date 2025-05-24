c6ca502ffd0d6d8a745e76014a9d89b09016260b
Ryo Igarashi <n33t5hin@gmail.com>
2025-05-24 12:19:13 +0900

    day07aまでやったつもり

--- kernel/error.hpp
+++ kernel/error.hpp
@@ -28,6 +28,7 @@ public:
 		kInvalidPhase,
 		kUnknownXHCISpeedID,
 		kNoWaiter,
+		kNoPCIMSI,
 		kLastOfCode, // この列挙子は常に最後に配置する
 	};
 
@@ -53,6 +54,7 @@ private:
 		"kInvalidPhase",
 		"kUnknownXHCISpeedID",
 		"kNoWaiter",
+		"kNoPCIMSI",
 	};
 	static_assert(Error::Code::kLastOfCode == code_names_.size());
 
