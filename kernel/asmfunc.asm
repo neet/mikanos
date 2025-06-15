6c47cb61af725466b5f257c03f03455496e4eb85
Ryo Igarashi <n33t5hin@gmail.com>
2025-06-15 13:29:56 +0900

    13.2まで

--- kernel/asmfunc.asm
+++ kernel/asmfunc.asm
@@ -86,3 +86,94 @@ global SetCR3
 SetCR3:
     mov cr3, rdi
     ret
+
+global GetCR3
+GetCR3:
+    mov rax, cr3
+    ret
+
+; rdi=第1引数, rsi=第2引数
+; `mov dst, src` 等号と同じ順番だと思ったほうがいい
+global SwitchContext
+SwitchContext:
+    mov [rsi + 0x40], rax
+    mov [rsi + 0x48], rbx
+    mov [rsi + 0x50], rcx
+    mov [rsi + 0x58], rdx
+    mov [rsi + 0x60], rdi
+    mov [rsi + 0x68], rsi
+
+    ; いまスタックの先頭は戻り先アドレス、つまり SwitchContext() の次の行になっている
+    ; ので、戻り先アドレスの分をスキップして、１個前のスタックを RAX に取り出す
+    lea rax, [rsp + 8]
+    ; それを task_a_ctx.rsp にいれる
+    mov [rsi + 0x70], rax
+    mov [rsi + 0x78], rbp
+
+    mov [rsi + 0x80], r8
+    mov [rsi + 0x88], r9
+    mov [rsi + 0x90], r10
+    mov [rsi + 0x98], r11
+    mov [rsi + 0xa0], r12
+    mov [rsi + 0xa8], r13
+    mov [rsi + 0xb0], r14
+    mov [rsi + 0xb8], r15
+
+
+    mov rax, cr3
+    mov [rsi + 0x00], rax
+    ; RSP には、 CALL によって戻り先アドレスが入っている。それを RAX に入れて
+    mov rax, [rsp] 
+    ; RAX を task_a_ctx.rip に入れる
+    mov [rsi + 0x08], rax
+    pushfq
+    pop qword [rsi + 0x10]
+
+    mov ax, cs
+    mov [rsi + 0x20], rax
+    mov bx, ss
+    mov [rsi + 0x28], rbx
+    mov cx, fs
+    mov [rsi + 0x30], rcx
+    mov dx, gs
+    mov [rsi + 0x30], rdx
+
+    fxsave [rsi + 0xc0]
+    
+    ; iret 用のスタックフレーム
+    ; RIP, CS, RFLAGS, RSP, SS に載っていてほしい情報をスタックに積んでおいて
+    ; そのあとで iret を実行すると、それが積まれた状態で呼び出し元に戻るらしい
+    push qword [rdi + 0x28]
+    push qword [rdi + 0x70]
+    push qword [rdi + 0x10]
+    push qword [rdi + 0x20]
+    push qword [rdi + 0x08]
+
+    ; コンテキストの復帰
+   fxrstor [rdi + 0xc0]
+
+   mov rax, [rdi + 0x00]
+   mov cr3, rax
+   mov rax, [rdi + 0x30]
+   mov fs, ax
+   mov rax, [rdi + 0x38]
+   mov gs, ax
+
+   mov rax, [rdi + 0x40]
+   mov rbx, [rdi + 0x48]
+   mov rcx, [rdi + 0x50]
+   mov rdx, [rdi + 0x58]
+   mov rsi, [rdi + 0x68]
+   mov rbp, [rdi + 0x78]
+   mov r8, [rdi + 0x80]
+   mov r9, [rdi + 0x88]
+   mov r10, [rdi + 0x90]
+   mov r11, [rdi + 0x98]
+   mov r12, [rdi + 0xa0]
+   mov r13, [rdi + 0xa8]
+   mov r14, [rdi + 0xb0]
+   mov r15, [rdi + 0xb8]
+
+   mov rdi, [rdi + 0x60]
+
+   o64 iret
